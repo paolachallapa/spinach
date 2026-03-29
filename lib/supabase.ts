22084281431ee567009dbd6f42adf5cf7ceed0fb
paolachallapa <challapacruzpaola@gmail.com>
2026-03-29 07:06:01 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qgrcsrvbmtudcwwnkjsn.supabase.co'
-// Pega aquí tu clave Anon Key real entre las comillas
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncmNzcnZibXR1ZGN3d25ranNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDA5NTEsImV4cCI6MjA5MDMxNjk1MX0.1-cjBKsMg4sTlA6WI9nPdWpHVWZZ9J1iK0QOIsa-A7ceyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncmNzcnZibXR1ZGN3d25ranNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDA5NTEsImV4cCI6MjA5MDMxNjk1MX0.1-cjBKsMg4sTlA6WI9nPdWpHVWZZ9J1iK0QOIsa-A7c' 
+// Esta es la clave corregida (sin repeticiones)
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncmNzcnZibXR1ZGN3d25ranNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDA5NTEsImV4cCI6MjA5MDMxNjk1MX0.1-cjBKsMg4sTlA6WI9nPdWpHVWZZ9J1iK0QOIsa-A7c'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
